cf781d53dd512f1ba6aa3dfab2a288e671e5cf4e
oliveira-helio <oliveira.helio.paulo@gmail.com>
2025-03-21 14:35:19 -0300

    update

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
 // const apiAdress=process.env.NODE_ENV === 'development' ? process.env.LOCAL_API_URL : process.env.NEXT_PUBLIC_API_URL;
-const apiAdress='http://localhost:5000';
+const apiAdress= (process.env.NODE_ENV === 'development' ? process.env.PUBLIC_API_URL : process.env.NEXT_PUBLIC_API_URL);
 
 
 export default apiAdress; 
