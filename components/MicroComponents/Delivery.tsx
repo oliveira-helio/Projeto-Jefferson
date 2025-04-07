9796fe36361568fe2d3750cc73e27816f47b32f5
oliveira-helio <oliveira.helio.paulo@gmail.com>
2025-04-07 20:40:28 -0300

    update

--- components/MicroComponents/Delivery.tsx
+++ components/MicroComponents/Delivery.tsx
@@ -122,10 +122,9 @@ const Delivery: React.FC<DeliveryProps> = ({
           <h3 className="text-lg font-medium my-2">Opções de frete:</h3>
           <ul className="flex flex-col gap-4">
             {deliveryOptions.map((option, index) => (
-              !option.name || !option.price || !option.delivery_time ?
+              !option.name || !option.price || !option.delivery_time  || (option.name.id !== 2 && option.name.id !== 4) ?
                 null
               :
-
                 <li key={index}>
                   <label>
                     <div className="w-full relative flex flex-row gap-4 justify-around">
